2f6b51fe3e98ab2d7aa909416520db2708a3119d
AchrafElGhazi <a.elghazi@aui.ma>
2025-01-03 00:46:46 +0100

    commit

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -228,7 +228,7 @@ const Join = () => {
               <motion.button
                 type='button'
                 onClick={nextStep}
-                className='bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50'
+                className='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50'
                 variants={buttonVariants}
                 whileHover='hover'
                 whileTap='tap'
